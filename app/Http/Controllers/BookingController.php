7bb865c3cc0c5825bc6d70050a17aa094f32013b
ammar habbab <ammarhabbab118@gmail.com>
2024-01-09 08:50:35 +0200

    Small Update

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -367,32 +367,6 @@ class BookingController extends Controller
     //     return response()->json(['message','cancel']);
     // }
 
-
-  
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
     /**
      * Remove the specified resource from storage.
      */
